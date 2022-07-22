f9d0cd801413cc23ea53909fff139c87695167ca
Toan Nguyen <nguyenductoan308@gmail.com>
2022-07-23 00:37:14 +0700

    Updates

---
+++ precache-manifest.6aa144a01e35bdfe3d933bde6bcf2df3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "736610961695b08c94b7",
+    "url": "/notes/static/css/main.b7c60c8a.chunk.css"
+  },
+  {
+    "revision": "736610961695b08c94b7",
+    "url": "/notes/static/js/main.73661096.chunk.js"
+  },
+  {
+    "revision": "bbec7dc8d58ec3f5adc9",
+    "url": "/notes/static/js/1.bbec7dc8.chunk.js"
+  },
+  {
+    "revision": "1291e1b612bc298497c9",
+    "url": "/notes/static/js/runtime~main.1291e1b6.js"
+  },
+  {
+    "revision": "7ba6cc24a63c72d3111bc0a50c135380",
+    "url": "/notes/static/media/image.7ba6cc24.jpg"
+  },
+  {
+    "revision": "ca2289ec1656d62080c918e6cd7179dc",
+    "url": "/notes/index.html"
+  }
+];
